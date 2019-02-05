097c0a2b8775652b1f763c1d763495f359edf011
Menol Razeek <cmrSriLanka@gmail.com>
2019-02-05 04:02:26 +0000

    Added NForNPricing strategy

---
+++ Solution/Chekout/NForNPricingStrategy.cs
@@ -0,0 +1,29 @@
+﻿using Chekout.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chekout
+{
+    class NForNPricingStrategy : IPricingStrategy
+    {
+        public string ApplicableItemType { get; set; }
+        public int SetSize { get; set; } 
+        public int PriceForASet { get; set; }
+
+        private static DummyPriceProvider dummyPriceProvider = new DummyPriceProvider();
+
+        public int GetSubTotal(IEnumerable<string> allItems)
+        {
+            int unitPrice = dummyPriceProvider.GetPrice(ApplicableItemType);
+            var noOfItemsOfThisType = allItems?.Count(item => item.Equals(ApplicableItemType, StringComparison.OrdinalIgnoreCase)) ?? 0;
+            
+            int subTotal = (noOfItemsOfThisType / SetSize) * unitPrice; // price for complete sets
+            subTotal += (noOfItemsOfThisType % SetSize) * unitPrice;  // price for any remaining items
+
+            return subTotal;
+        }
+    }
+}
