c34e3da4f0e90e9c9305ecf74f7a5408afb04f15
Menol Razeek <cmrSriLanka@gmail.com>
2019-02-05 02:48:38 +0000

    added basic structure to the solution

---
+++ Solution/Chekout/BasicCheckout.cs
@@ -0,0 +1,22 @@
+﻿using Chekout.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chekout
+{
+    class BasicCheckout : ICheckout
+    {
+        public int GetTotalPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(string item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Solution/Chekout/PricingEngine.cs
@@ -0,0 +1,24 @@
+﻿using Chekout.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chekout
+{
+    class PricingEngine
+    {
+        private List<IPricingStrategy> pricingStrategies; 
+
+        public void AddItem(string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetTotal()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
